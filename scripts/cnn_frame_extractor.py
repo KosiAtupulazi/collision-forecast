a1bed7ac8c0f0fb2058299895bcfa53ca6527786
ColetteAtupulazi <colette.atupulazi@my.utsa.edu>
2025-07-01 21:49:19 -0500

    created the npy files

--- scripts/cnn_frame_extractor.py
+++ scripts/cnn_frame_extractor.py
@@ -1,3 +1,4 @@
+
 # %%
 import os
 import cv2
@@ -9,10 +10,10 @@ import numpy as np
 # %%
 for dataset_split in ['train', 'val', 'test']:
     print(f"Processing {dataset_split} split...")
-    main_dir = "/home/atupulazi/personal_projects/collision-detection/processed_dataset"
+    main_dir = "/home/atupulazi/personal_projects/collision-forecast/final_dataset"
     video_dir = os.path.join(main_dir, dataset_split)
     csv_path = os.path.join(main_dir, f"{dataset_split}.csv")
-    output_dir = os.path.join("frames", dataset_split)
+    output_dir = os.path.join("/home/atupulazi/personal_projects/collision-forecast/frames", dataset_split)
 
     #margin = 1
     #how to print files and debug if i get errors???
@@ -21,7 +22,7 @@ for dataset_split in ['train', 'val', 'test']:
     clip_labels = [] #output of extracted clips and their labels; will write to csv later
     print(df.head())
 
-    for event in ['crash', 'no_crash']: #
+    for event in ['crash']: 
         eventfolder_path = os.path.join(video_dir, event) #the path to each event
 
         for filename in os.listdir(eventfolder_path):
@@ -39,8 +40,6 @@ for dataset_split in ['train', 'val', 'test']:
                 continue #if the id is not in the csv, skip it
             df_row = df_row.iloc[0] # gets the first matching row from the dataframe
 
-
-
             #open video
             video_capture = cv2.VideoCapture(event_video)
 
@@ -53,10 +52,13 @@ for dataset_split in ['train', 'val', 'test']:
             #how many consecutive clips at a time
             clip_len = 16
 
-            #if we know where the crash occured, extract 16 frames around that moment (8 before the center and 8 after)
-            if df_row["label"] == "crash" and pd.notna(df_row["time_of_event"]):
-                center_time = float(df_row['time_of_event']) #crash time saved in the dataframe
-                center_frame = int(center_time * fps) #tells the frame where the crash likely happened ie. 22.752 * 30 = 682.56 → int(682.56) = 682
+            #if we know where the system is supposed to alert the driver, extract 16 frames around that moment (8 before the center and 8 after)
+            if df_row["label"] == "crash" and pd.notna(df_row["time_of_alert"]):
+                center_time = float(df_row['time_of_alert']) #crash time saved in the dataframe
+                forecast_time = center_time - 2 # 2 seconds before the crash
+                if forecast_time <= 0:
+                    continue # skip if the video is too short
+                center_frame = int(forecast_time * fps) #tells the frame where the crash likely happened ie. 22.752 * 30 = 682.56 → int(682.56) = 682
                 start_frame = max(0, center_frame - clip_len // 2) #start 8 frames before the crash ie center_frame - 8 = 682 - 8 = 674
                 end_frame = min(total_frames, start_frame + clip_len) #begin from the start frame to the 16th clip ie so we end at 674 + 16 = 690
                 clip_indices = list(range(start_frame, end_frame)) #a list of the the frames of curr vid range(674, 690) → [674, 675, 676, ..., 689]
@@ -76,7 +78,7 @@ for dataset_split in ['train', 'val', 'test']:
                 if not success:
                     print(f"[WARNING] Failed to read frame {frame_idx} in {filename}")
                     continue #if not successful in reading the frame, skip it
-                frame = cv2.resize(frame, (112,112)) #resize to 224x224 pixels (input size for CNNs on videos)
+                frame = cv2.resize(frame, (224,224)) #resize to 224x224 pixels (input size for videomae on videos)
                 # opencv = (height, width, channels) → (112, 112, 3)
                 #pytorch = (channels, height, width) → (3, 112, 112)
                 frame = frame.transpose(2, 0, 1) #converts the opencv format to pytorch format 9swaps the dimensions)
@@ -91,6 +93,8 @@ for dataset_split in ['train', 'val', 'test']:
                 os.makedirs(save_dir, exist_ok=True) #creates the folder if it doesn’t exist.
                 clip_name = f"{raw_id}_clip.npy" #raw_id = "00822" → clip_name = "00822_clip.npy"
                 save_path = os.path.join(save_dir, clip_name) #Full file path where the tensor will be saved.
+                print(f"Saving to: {save_path}") #debug 
+
                 np.save(save_path, tensor) #actually saves the tensor to disk as a .npy file 
 
                 clip_labels.append((clip_name, df_row["label"])) # a list of tuples to track what label goes with which clip.
