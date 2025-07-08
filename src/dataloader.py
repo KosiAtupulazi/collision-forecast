4858aacf7e155fc9d9cb5b2488395c909448fbb3
Colette <colette.atupulazi@my.utsa.edu>
2025-07-08 10:25:20 -0500

    edited dataloader

--- src/dataloader.py
+++ src/dataloader.py
@@ -7,7 +7,7 @@ import os
 import numpy as np
 
 #self: memory of this dataset
-class CollisionForecastDataset(torch.utils.data.Dataset):
+class CollisionForecastDataset(Dataset):
     def __init__(self, csv_path, split='train', transform=None):
         self.data = pd.read_csv(csv_path)
         self.split = split
@@ -20,6 +20,13 @@ class CollisionForecastDataset(torch.utils.data.Dataset):
         # builds the full file path by combining the label (as folder) and clip name, e.g., 'frames/crash/00822_clip.npy' from the csv
         clip_path = os.path.join(project_root, 'frames', self.split, row['label'], row['clip_name']) 
         clip = np.load(clip_path) #loading the npy files
+        
+        # Normalize using ImageNet pretrained VideoMAE stats
+        mean = np.array([0.485, 0.456, 0.406]).reshape(3, 1, 1, 1)
+        std = np.array([0.229, 0.224, 0.225]).reshape(3, 1, 1, 1)
+        
+        clip = clip / 255.0  # Scale from [0,255] to [0,1]
+        clip = (clip - mean) / std  # Normalize channel-wise
 
         #apply the transformation if its set
         if self.transform:
